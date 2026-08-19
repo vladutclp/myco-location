bc281e9994215288d2006e19ac06e5ddb17b088a
vladutclp <vladut.clapou@gmail.com>
2026-08-19 11:46:10 +0300

    Created custom hook for session token handling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,29 +4,24 @@ import Layout from "./pages/Layout";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
-import AuthProvider from "./store/auth-context";
 import Spots from "./pages/Spots";
 import ProtectedRoute from "./routes/ProtectedRoute";
-
+import useSessionToken from "./hooks/useSessionToken";
 function App() {
+  useSessionToken();
   return (
-    <AuthProvider>
-      <Layout>
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route element={<ProtectedRoute />}>
-            <Route path="spots" element={<Spots />} />
-            <Route path="new-spot" element={<div>New spot component</div>} />
-          </Route>
-          <Route
-            path="*"
-            element={<div>Sorry, this route does not exist</div>}
-          />
-        </Routes>
-      </Layout>
-    </AuthProvider>
+    <Layout>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route element={<ProtectedRoute />}>
+          <Route path="spots" element={<Spots />} />
+          <Route path="new-spot" element={<div>New spot component</div>} />
+        </Route>
+        <Route path="*" element={<div>Sorry, this route does not exist</div>} />
+      </Routes>
+    </Layout>
   );
 }
 
